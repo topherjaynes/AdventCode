020e495f1501e2b135873138178c092bd0c3d8bb
Christopher Jaynes <chris.jaynes@gmail.com>
2024-12-04 06:05:18 -0500

    starting day 4

---
+++ Day4/day4a.py
@@ -0,0 +1,40 @@
+'''
+"Looks like the Chief's not here. Next!" One of The Historians pulls out a device and pushes the only button on it. After a brief flash, you recognize the interior of the Ceres monitoring station!
+
+As the search for the Chief continues, a small Elf who lives on the station tugs on your shirt; she'd like to know if you could help her with her word search (your puzzle input). She only has to find one word: XMAS.
+
+This word search allows words to be horizontal, vertical, diagonal, written backwards, or even overlapping other words. It's a little unusual, though, as you don't merely need to find one instance of XMAS - you need to find all of them. Here are a few ways XMAS might appear, where irrelevant characters have been replaced with .:
+
+
+..X...
+.SAMX.
+.A..A.
+XMAS.S
+.X....
+The actual word search will be full of letters instead. For example:
+
+MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX
+In this word search, XMAS occurs a total of 18 times; here's the same word search again, but where letters not involved in any XMAS have been replaced with .:
+
+
+'''
+
+
+def wordsearch(file_path):
+    #open the file and store in a grid. Assuming it's a uniform, but think through edgecases
+
+
+
+    #word directions. It can go any direction like a word search
+
+
+    #cound occurences, is it a double for loop?
