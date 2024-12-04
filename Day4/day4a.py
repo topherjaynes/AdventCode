d1b85782cdba510bcbe1e700f81c8be9b51c03a6
Christopher Jaynes <chris.jaynes@gmail.com>
2024-12-04 06:13:59 -0500

    Scaffold for day4

--- Day4/day4a.py
+++ Day4/day4a.py
@@ -29,12 +29,46 @@ In this word search, XMAS occurs a total of 18 times; here's the same word searc
 '''
 
 
-def wordsearch(file_path):
+def wordsearch(file_path, word):
     #open the file and store in a grid. Assuming it's a uniform, but think through edgecases
-
-
+    with open(file_path, 'r') as file:
+        grid = [line.strip() for line in file.readlines()]
 
     #word directions. It can go any direction like a word search
+    directions = [
+        (0, 1),  # Right
+        (0, -1), # Left
+        (1, 0),  # Down
+        (-1, 0), # Up
+        (1, 1),  # Diagonal Down-Right
+        (1, -1), # Diagonal Down-Left
+        (-1, 1), # Diagonal Up-Right
+        (-1, -1) # Diagonal Up-Left
+    ]
+    word_length = len(word)
+    rows, cols = len(grid), len(grid[0])
 
 
-    #cound occurences, is it a double for loop?
+    #seperated the function to check if word exisits in each directions
+    #brute force first and then check to see if the letter we're on is the first of Word
+    # if not then move on, no need to check directions
+    def check_directions(x,y,dx,dy):
+        for i in range(word_length):
+            nx, ny = x + 1 * dx,y + i *dy
+            if nx < 0 or nx >= rows or ny < 0 or ny >= cols or grid[nx][ny] != word[i]:
+                return False
+        return True
+
+    #count occurences, is it a double for loop?
+    count = 0
+    for x in range(rows):
+        for y in range(cols):
+            #moving across all the cols first
+            #check directions from coordinate
+            for dx, dy in directions:
+                if check_directions(x, y, dx,dy)
+                    count +=1
+    return count
+
+result = wordsearch('/Users/topherjaynes/Desktop/AdventCode/Day4/input4.txt', 'XMAS')
+print("Total occurrences of XMAS:", result)
